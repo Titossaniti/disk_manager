022d5aa84281f65f801689042ca1d3f4ec8aa877
Titossaniti <tristan.erard@gmail.com>
2025-06-24 13:00:51 +0200

    app/page modified + auth)/page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,3 @@
-"use client"
-
-import { useAuth } from "@/hooks/useAuth"
-import { useRouter } from "next/navigation"
-import { useEffect } from "react"
-
-export default function Home() {
-    const { user, isLoading } = useAuth()
-    const router = useRouter()
-
-    useEffect(() => {
-        if (!isLoading) {
-            if (user) {
-                router.push("/home")
-            } else {
-                router.push("/login")
-            }
-        }
-    }, [user, isLoading, router])
-
-    return null
+export default function RedirectRoot() {
+    return null;
 }
