dde1eb32907b52e88ac1a60f89221344e572f484
Titossaniti <tristan.erard@gmail.com>
2025-06-25 11:29:56 +0200

    Head tag added in global layout html part / theme-provider client side component added (to make mobile and tablet device able to see the app style)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,12 +18,35 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html lang="fr" suppressHydrationWarning>
-            <body className={inter.className}>
-                <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-                    {children}
-                    <Toaster richColors closeButton position="top-right" />
-                </ThemeProvider>
-            </body>
+        <head>
+            <meta charSet="UTF-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <meta name="color-scheme" content="light dark"/>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+              (function() {
+                try {
+                  var theme = localStorage.getItem('theme');
+                  var systemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+                  if (theme === 'dark' || (!theme && systemDark)) {
+                    document.documentElement.classList.add('dark');
+                  } else {
+                    document.documentElement.classList.remove('dark');
+                  }
+                } catch (e) {}
+              })();
+            `
+                }}
+            />
+            <title>Disk Manager</title>
+        </head>
+        <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+            <Toaster richColors closeButton position="top-right"/>
+        </ThemeProvider>
+        </body>
         </html>
     )
 }
