a70fea7d8248930388304ccbe1c4ccf6bb27f190
Titossaniti <tristan.erard@gmail.com>
2025-06-25 12:12:27 +0200

    tailwind.config.ts added for mobile theme

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -3,18 +3,28 @@
 import { useTheme } from "next-themes"
 import { Button } from "@/components/ui/button"
 import { Moon, Sun } from "lucide-react"
+import { useEffect, useState } from "react"
 
 export function ThemeToggle() {
-    const { theme, setTheme } = useTheme()
+    const { resolvedTheme, setTheme } = useTheme()
+    const [mounted, setMounted] = useState(false)
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) return null
+
+    const isLight = resolvedTheme === "light"
 
     return (
         <Button
             variant="outline"
             size="icon"
-            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-            className={"cursor-pointer"}
+            onClick={() => setTheme(isLight ? "dark" : "light")}
+            className="cursor-pointer"
         >
-            {theme === "light" ? <Moon className="h-5 w-5" /> : <Sun className="h-5 w-5" />}
+            {isLight ? <Moon className="h-5 w-5" /> : <Sun className="h-5 w-5" />}
             <span className="sr-only">Changer de thème</span>
         </Button>
     )
